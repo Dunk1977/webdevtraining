bf69141ef390291b11311d7d63ee054281249509
Amad Uddin <amad.uddin@gmail.com>
2024-01-29 17:13:26 +0300

    drum kit completed

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -4,13 +4,23 @@ var drumButtons = document.querySelectorAll(".drum")
 
 for (var i = 0; i < drumButtons.length; i++)
 {
-    drumButtons[i].addEventListener("click", handleClick);
+    drumButtons[i].addEventListener("click", function () {
+        playSound(this.innerHTML)
+    })
+
 }
 
-function handleClick() {
-    // console.log(this.innerHTML);
+function playSound(sname) {
+    // console.log(sname);
 
-    switch(this.innerHTML) {
+    var activeButton = document.querySelector("."+sname);
+    activeButton.classList.add("pressed");
+    
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100)
+
+    switch(sname) {
         case "w":
             var audio = new Audio("./sounds/tom-1.mp3");
             break;
@@ -43,52 +53,11 @@ function handleClick() {
         }
     // var audio = new Audio("./sounds/tom-1.mp3");
     audio.play();
+    // activeButton.classList.remove("pressed");
+
 }
 
-
-// switch (expression) {
-//     case value1:
-//       // Code to be executed if expression matches value1
-//       break;
-//     case value2:
-//       // Code to be executed if expression matches value2
-//       break;
-//     // Additional cases can be added as needed
-//     default:
-//       // Code to be executed if none of the cases match the expression
-//   }
-  
-// document.querySelector(".w").addEventListener("click", function(){
-//     var audio = new Audio("./sounds/tom-1.mp3");
-//     audio.play();
-// })
-
-// document.querySelector(".a").addEventListener("click", function(){
-//     var audio = new Audio("./sounds/tom-2.mp3");
-//     audio.play()
-// })
-
-// document.querySelector(".s").addEventListener("click", function(){
-//     var audio = new Audio("./sounds/tom-3.mp3");
-//     audio.play();
-// })
-
-// document.querySelector(".d").addEventListener("click", function(){
-//     var audio = new Audio("./sounds/tom-4.mp3");
-//     audio.play();
-// })
-
-// document.querySelector(".j").addEventListener("click", function(){
-//     var audio = new Audio("./sounds/crash.mp3");
-//     audio.play();
-// })
-
-// document.querySelector(".k").addEventListener("click", function(){
-//     var audio = new Audio("./sounds/kick-bass.mp3");
-//     audio.play();
-// })
-
-// document.querySelector(".l").addEventListener("click", function(){
-//     var audio = new Audio("./sounds/snare.mp3");
-//     audio.play();
-// })
+document.addEventListener("keydown", function(event) {
+    // console.log(event.key);
+    playSound(event.key);
+})
