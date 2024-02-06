38e0458f5493fcda9f800c9d1f0b10e0fb94a33f
Amad Uddin <amad.uddin@gmail.com>
2024-02-06 11:10:15 +0300

    some more

---
+++ backend/2.3-NPM/index.js
@@ -0,0 +1,18 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyName";
+// var sillyName = generateName();
+
+// console.log(`My Name is ${sillyName}.`);
+
+
+
+import superheroes from "superheroes";
+
+const sh = superheroes.random();
+console.log(`I am ${sh}.`);
+ 
+// superheroes.all;
+// //=> ['3-D Man', 'A-Bomb', …]
+ 
+// superheroes.random();
